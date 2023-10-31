6f0df61b94ec6fa90f4606f044fec558c5cd229a
Srinivas Raman <srinivasraman18@gmail.com>
2023-10-30 19:05:38 -0700

    Added JSDocs

--- src/stretchtext.js
+++ src/stretchtext.js
@@ -1,127 +1,139 @@
-// Removed IIFE
-const TITLE_WHEN_CLOSED = 'Expand'
-const TITLE_WHEN_OPEN = 'Collapse'
-
-// requestAnimationFrame shimming.
-// Shimming is necessary when you want to use modern JavaScript features or APIs that are not supported in older browsers
-const requestAnimationFrame =
-      window.requestAnimationFrame || // Standard
-      window.webkitRequestAnimationFrame || // Safari, Chrome
-      window.mozRequestAnimationFrame || // Firefox
-      window.oRequestAnimationFrame || // Opera
-      window.msRequestAnimationFrame || // Internet Explorer
-      function (callback) {
-        // This is the fallback method if none of the above are available
-        const sixtyFPS = 1000 / 60
-        window.setTimeout(callback, sixtyFPS)
-      }
-
-function toggleSummary (evt) {
+/**
+ * Toggle the summary element to expand or collapse its associated details.
+ * @param {Event} evt - The event object triggered by the user's interaction.
+ */
+function toggleSummary(evt) {
   // Prevent the text from being selected if rapidly clicked.
-  evt.preventDefault()
+  evt.preventDefault();
 
-  const summary = evt.target
-  const detail = findDetailFor(summary)
-  if (!detail) { return }
+  const summary = evt.target;
+  const detail = findDetailFor(summary);
+  if (!detail) {
+    return;
+  }
 
   // CSS Transitions don't work as expected on things set to 'display: none'. Make the
   // stretch details visible if needed, then use a timeout for the transition to take
   // effect.
   if (summary.classList.contains('stretchtext-open')) {
     // If it is already open, there is no longer need for any fancy displays
-    detail.style.display = 'none'
+    detail.style.display = 'none';
   } else {
-    detail.style.display = isBlockLevelDetail(summary) ? 'block' : 'inline'
+    detail.style.display = isBlockLevelDetail(summary) ? 'block' : 'inline';
   }
 
   requestAnimationFrame(function () {
-    summary.classList.toggle('stretchtext-open')
-    detail.classList.toggle('stretchtext-open')
+    summary.classList.toggle('stretchtext-open');
+    detail.classList.toggle('stretchtext-open');
 
     if (summary.classList.contains('stretchtext-open')) {
-      setTitle(summary, TITLE_WHEN_OPEN)
+      setTitle(summary, TITLE_WHEN_OPEN);
     } else {
-      setTitle(summary, TITLE_WHEN_CLOSED)
+      setTitle(summary, TITLE_WHEN_CLOSED);
     }
-  })
+  });
 }
 
-function isBlockLevelDetail (summary) {
-  return summary.nodeName.toLowerCase() === 'a'
+/**
+ * Check if the given summary element represents a block-level detail.
+ * @param {Element} summary - The summary element to check.
+ * @returns {boolean} - `true` if the summary is a block-level detail, `false` otherwise.
+ */
+function isBlockLevelDetail(summary) {
+  return summary.nodeName.toLowerCase() === 'a';
 }
 
-function setTitle (summary, title) {
+/**
+ * Set the title attribute of a summary element.
+ * @param {Element} summary - The summary element to set the title for.
+ * @param {string} title - The title to set.
+ */
+function setTitle(summary, title) {
   // If the user placed a manual title on the summary leave it alone.
   // REMOVED REDUNDANT IF/ELSE
   if (!summary.hasAttribute('title')) {
-    summary.setAttribute('title', title)
+    summary.setAttribute('title', title);
   }
 }
 
-function findDetailFor (summary) {
+/**
+ * Find the associated details element for a given summary element.
+ * @param {Element} summary - The summary element to find the associated details for.
+ * @returns {Element|null} - The associated details element, or `null` if not found.
+ */
+function findDetailFor(summary) {
   // STREAMLINED TO ONE RETURN STATEMENT
-  let detail
+  let detail;
   if (isBlockLevelDetail(summary)) {
     // hrefs always start with #, need to remove them
-    const id = summary.getAttribute('href').replace(/^#/, '')
-    detail = document.getElementById(id)
+    const id = summary.getAttribute('href').replace(/^#/, '');
+    detail = document.getElementById(id);
     if (!detail && window.console) {
-      console.error('No StretchText details element with ID: ' + id)
+      console.error('No StretchText details element with ID: ' + id);
     }
   } else {
-    detail = summary.nextElementSibling
+    detail = summary.nextElementSibling;
     if (!detail && window.console) {
-      console.error('No StretchText details element found for: ', summary)
+      console.error('No StretchText details element found for: ', summary);
     }
   }
-  return detail
+  return detail;
 }
 
-function getSummaries () {
+/**
+ * Get all summary elements with a specific data attribute.
+ * @returns {Array<Element>} - An array of summary elements with the specified data attribute.
+ */
+function getSummaries() {
   // Summaries are the thing that you click to expand
-  const results = []
-  let summaries // Initialize variable
+  const results = [];
+  let summaries; // Initialize variable
 
-  summaries = document.querySelectorAll('[data-epub-type="stretchsummary"]')
+  summaries = document.querySelectorAll('[data-epub-type="stretchsummary"]');
   Array.prototype.forEach.call(summaries, function (result) {
-    results.push(result)
-  })
+    results.push(result);
+  });
 
-  // // CSS class.
-  // summaries = document.getElementsByClassName('stretchsummary')
-  // Array.prototype.forEach.call(summaries, function (result) {
-  //   results.push(result)
-  // })
-
-  return results
+  return results;
 }
 
-let loadedCalled = false
-function loaded () {
-  if (loadedCalled) { return }
-  loadedCalled = true
+/**
+ * Initialize the functionality when the DOM content is loaded.
+ */
+function loaded() {
+  if (loadedCalled) {
+    return;
+  }
+  loadedCalled = true;
   // FIXME(slightlyoff): Add global handlers instead of one per item.
   getSummaries().forEach(function (summary) {
-    summary.setAttribute('title', TITLE_WHEN_CLOSED)
+    summary.setAttribute('title', TITLE_WHEN_CLOSED);
 
     // Listen on mousedown instead of click so that we can prevent text
-    // selection if mouse is clicked rapidly.
-    summary.addEventListener('mousedown', toggleSummary)
+    // selection if the mouse is clicked rapidly.
+    summary.addEventListener('mousedown', toggleSummary);
 
-    summary.addEventListener('touchstart', toggleSummary)
+    summary.addEventListener('touchstart', toggleSummary);
 
-    // Link resolving can't be canceled in mousedown event, only in click
+    // Link resolving can't be canceled in mousedown event, only in the click
     // event.
-    summary.addEventListener('click', function (e) { e.preventDefault() })
-  })
+    summary.addEventListener('click', function (e) { e.preventDefault(); });
+  });
 }
 
-window.addEventListener('DOMContentLoaded', loaded)
+/**
+ * Event handler for when the DOM content is fully loaded.
+ */
+function onDOMContentLoaded() {
+  loaded();
+}
+
+window.addEventListener('DOMContentLoaded', onDOMContentLoaded);
+
 if (document.readyState === 'complete') {
-  loaded()
+  loaded();
 }
 
-if (typeof module == 'object')
-{
-  module.exports = {getSummaries, setTitle, isBlockLevelDetail, findDetailFor}
+if (typeof module === 'object') {
+  module.exports = { getSummaries, setTitle, isBlockLevelDetail, findDetailFor };
 }
